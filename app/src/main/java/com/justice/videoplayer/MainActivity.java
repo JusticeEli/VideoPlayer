a79f6bc9f9f1b9350cfae7015c598f27416fca97
JusticeEli <justelonelin@gmail.com>
2020-06-09 17:44:34 +0300

    updated main page animations

--- app/src/main/java/com/justice/videoplayer/MainActivity.java
+++ app/src/main/java/com/justice/videoplayer/MainActivity.java
@@ -1,13 +1,9 @@
 package com.justice.videoplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Gravity;
@@ -15,56 +11,42 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-import com.flaviofaria.kenburnsview.KenBurnsView;
-import com.universalvideoview.UniversalVideoView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private GestureDetector gestureDetector;
     private static final String TAG = "MainActivity";
+    //widgets//
     private ProgressBar progressBar;
     private VideoView videoView;
-    private List<Uri> uriList;
 
-    private KenBurnsView video_1;
-    private KenBurnsView video_2;
-    private KenBurnsView video_3;
-    private KenBurnsView video_4;
+    private List<Uri> uriList; //stores online urls
 
-    private Handler progressHandler = new Handler();
-    private Runnable progressRunnable = new Runnable() {
-        @Override
-        public void run() {
-            progressBar.setProgress(videoView.getCurrentPosition());
-            progressHandler.postDelayed(this, 3);
-
-        }
-    };
-
-
-    public int nextSong;
-    private int songCounter;
+    public int nextSong;//determines currently playing song
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //    startActivity(new Intent(this,TestActivity.class));
         initWidgets();
         addVideosToList();
         setUpVideoView();
         setOnSwipeListener();
     }
 
+    private void initWidgets() {
+        progressBar = findViewById(R.id.progressBar);
+        videoView = findViewById(R.id.videoView);
+
+    }
 
     private void addVideosToList() {
         String video_1 = "http://videocdn.bodybuilding.com/video/mp4/62000/62792m.mp4";
@@ -79,91 +61,69 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     }
 
-    private void setOnSwipeListener() {
-        videoView.setOnTouchListener(this);
-        gestureDetector = new GestureDetector(this, new OnSwipeListener(this));
-
-    }
-
-
-    private void initWidgets() {
-        progressBar = findViewById(R.id.progressBar);
-        videoView = findViewById(R.id.videoView);
-
-        video_1 = findViewById(R.id.video_1);
-        video_2 = findViewById(R.id.video_2);
-        video_3 = findViewById(R.id.video_3);
-        video_4 = findViewById(R.id.video_4);
-
-        video_1.pause();
-        video_2.pause();
-        video_3.pause();
-        video_4.pause();
-
-    }
-
     public void setUpVideoView() {
-        nextSong = songCounter++ % uriList.size(); //go to the next song
-
-
-        videoView = findViewById(R.id.videoView);
         videoView.setVideoURI(uriList.get(nextSong));
-
         progressBar.setVisibility(View.VISIBLE);
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 progressBar.setVisibility(View.GONE);
                 videoView.start();
-                animateVideoThatsPlaying();
+
 
             }
         });
 
+    }
 
-      /*  videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+    private void setOnSwipeListener() {
+        videoView.setOnTouchListener(this);
+        gestureDetector = new GestureDetector(this, new OnSwipeListener(this));
 
-            @Override
-            public void onCompletion(MediaPlayer mp) {
+    }
 
-                videoView.setVideoURI(uriList.get(nextSong));
-                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mp) {
-                        videoView.start();
-                    }
-                });
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return true;
+    }
 
-                startProgressBar();
-                animateVideoThatsPlaying();
+    ///Handles the gestures when user swipes on the screen
+    public void onSwipe(OnSwipeListener.Direction direction) {
+
+        Log.d(TAG, "onSwipe: ");
+
+        if (direction == OnSwipeListener.Direction.up) {
+
+            //CHOOSE RANDOM VIDEO BETWEEN THE TWO VIDEOS ABOVE THE THE PLAYING VIDEO
+            if (nextSong == 2) {
+                nextSong = 3;
+            } else {
+                nextSong = 2;
             }
-        });*/
-    }
+            setUpVideoView();
+        } else if (direction == OnSwipeListener.Direction.down) {
 
-    private void animateVideoThatsPlaying() {
-        switch (nextSong) {
-            case 1:
-                video_1.resume();
-                break;
-            case 2:
-                video_2.resume();
-                break;
-            case 3:
-                video_3.resume();
-                break;
-            case 4:
-                video_4.resume();
-                break;
 
+            //CHOOSE RANDOM VIDEO BETWEEN THE TWO VIDEOS BELOW THE THE PLAYING VIDEO
+            if (nextSong == 0) {
+                nextSong = 1;
+            } else {
+                nextSong = 0;
+            }
+            setUpVideoView();
+
+
+        } else if (direction == OnSwipeListener.Direction.right) {
+            startActivity(new Intent(this, ProfileActivity.class));
+        } else if (direction == OnSwipeListener.Direction.left) {
+            View view = LayoutInflater.from(this).inflate(R.layout.toast_image, (ViewGroup) findViewById(R.id.linearLayout), false);
+            Toast toast = Toast.makeText(this, null, Toast.LENGTH_SHORT);
+            toast.setView(view);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.show();
         }
-    }
-
-    private void startProgressBar() {
-        progressBar.setProgress(0);
-        progressBar.setMax(videoView.getDuration());
-        progressHandler.post(progressRunnable);
-
-
     }
 
     @Override
@@ -182,47 +142,5 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
 
     }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        gestureDetector.onTouchEvent(event);
-        return true;
-    }
-
-    public void onSwipe(OnSwipeListener.Direction direction) {
-
-        Log.d(TAG, "onSwipe: ");
-
-        if (direction == OnSwipeListener.Direction.up) {
-
-            //CHOOSE RANDOM VIDEO BETWEEN THE TWO VIDEOS ABOVE THE THE PLAYING VIDEO
-            if (nextSong == 2) {
-                nextSong = 3;
-            } else {
-                nextSong = 2;
-            }
-            setUpVideoView();
-
-        } else if (direction == OnSwipeListener.Direction.down) {
-
-            //CHOOSE RANDOM VIDEO BETWEEN THE TWO VIDEOS BELOW THE THE PLAYING VIDEO
-            if (nextSong == 0) {
-                nextSong = 1;
-            } else {
-                nextSong = 0;
-            }
-            setUpVideoView();
-
-        } else if (direction == OnSwipeListener.Direction.right) {
-            startActivity(new Intent(this, ProfileActivity.class));
-        } else if (direction == OnSwipeListener.Direction.left) {
-            View view = LayoutInflater.from(this).inflate(R.layout.toast_image, (ViewGroup) findViewById(R.id.linearLayout), false);
-            Toast toast = Toast.makeText(this, null, Toast.LENGTH_SHORT);
-            toast.setView(view);
-            toast.setGravity(Gravity.CENTER, 0, 0);
-            toast.show();
-        }
-    }
-
 
 }
--- app/src/main/java/com/justice/videoplayer/OnSwipeListener.java
+++ app/src/main/java/com/justice/videoplayer/OnSwipeListener.java
@@ -8,7 +8,7 @@ import android.view.MotionEvent;
 public class OnSwipeListener implements GestureDetector.OnGestureListener {
     private Context context;
 
-    private static final String TAG="OnSwipeListener";
+    private static final String TAG = "OnSwipeListener";
 
     public OnSwipeListener(Context context) {
         this.context = context;
@@ -29,24 +29,12 @@ public class OnSwipeListener implements GestureDetector.OnGestureListener {
     public boolean onSingleTapUp(MotionEvent e) {
         Log.d(TAG, "onSingleTapUp: ");
         return false;
-        
+
     }
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
         Log.d(TAG, "onScroll: ");
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-        Log.d(TAG, "onLongPress: ");
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
         // Grab two events located on the plane at e1=(x1, y1) and e2=(x2, y2)
         // Let e1 be the initial event
         // e2 can be located at 4 different positions, consider the following diagram
@@ -72,33 +60,47 @@ public class OnSwipeListener implements GestureDetector.OnGestureListener {
         float x2 = e2.getX();
         float y2 = e2.getY();
 
-        Direction direction = getDirection(x1,y1,x2,y2);
+        Direction direction = getDirection(x1, y1, x2, y2);
         onSwipe(direction);
         return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Log.d(TAG, "onLongPress: ");
 
     }
 
-    /** Override this method. The Direction enum will tell you how the user swiped. */
-    public void onSwipe(Direction direction){
-        ((MainActivity)context).onSwipe(direction);
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.d(TAG, "onFling: ");
+        return false;
+
+    }
+
+    /**
+     * Override this method. The Direction enum will tell you how the user swiped.
+     */
+    public void onSwipe(Direction direction) {
+        ((MainActivity) context).onSwipe(direction);
     }
 
     /**
      * Given two points in the plane p1=(x1, x2) and p2=(y1, y1), this method
      * returns the direction that an arrow pointing from p1 to p2 would have.
+     *
      * @param x1 the x position of the first point
      * @param y1 the y position of the first point
      * @param x2 the x position of the second point
      * @param y2 the y position of the second point
      * @return the direction
      */
-    public Direction getDirection(float x1, float y1, float x2, float y2){
+    public Direction getDirection(float x1, float y1, float x2, float y2) {
         double angle = getAngle(x1, y1, x2, y2);
         return Direction.fromAngle(angle);
     }
 
     /**
-     *
      * Finds the angle between two points in the plane (x1,y1) and (x2, y2)
      * The angle is measured with 0/360 being the X-axis to the right, angles
      * increase counter clockwise.
@@ -111,12 +113,12 @@ public class OnSwipeListener implements GestureDetector.OnGestureListener {
      */
     public double getAngle(float x1, float y1, float x2, float y2) {
 
-        double rad = Math.atan2(y1-y2,x2-x1) + Math.PI;
-        return (rad*180/Math.PI + 180)%360;
+        double rad = Math.atan2(y1 - y2, x2 - x1) + Math.PI;
+        return (rad * 180 / Math.PI + 180) % 360;
     }
 
 
-    public enum Direction{
+    public enum Direction {
         up,
         down,
         left,
@@ -125,7 +127,7 @@ public class OnSwipeListener implements GestureDetector.OnGestureListener {
         /**
          * Returns a direction given an angle.
          * Directions are defined as follows:
-         *
+         * <p>
          * Up: [45, 135]
          * Right: [0,45] and [315, 360]
          * Down: [225, 315]
@@ -134,17 +136,14 @@ public class OnSwipeListener implements GestureDetector.OnGestureListener {
          * @param angle an angle from 0 to 360 - e
          * @return the direction of an angle
          */
-        public static Direction fromAngle(double angle){
-            if(inRange(angle, 45, 135)){
+        public static Direction fromAngle(double angle) {
+            if (inRange(angle, 45, 135)) {
                 return Direction.up;
-            }
-            else if(inRange(angle, 0,45) || inRange(angle, 315, 360)){
+            } else if (inRange(angle, 0, 45) || inRange(angle, 315, 360)) {
                 return Direction.right;
-            }
-            else if(inRange(angle, 225, 315)){
+            } else if (inRange(angle, 225, 315)) {
                 return Direction.down;
-            }
-            else{
+            } else {
                 return Direction.left;
             }
 
@@ -152,10 +151,12 @@ public class OnSwipeListener implements GestureDetector.OnGestureListener {
 
         /**
          * @param angle an angle
-         * @param init the initial bound
-         * @param end the final bound
+         * @param init  the initial bound
+         * @param end   the final bound
          * @return returns true if the given angle is in the interval [init, end).
          */
-        private static boolean inRange(double angle, float init, float end){
+        private static boolean inRange(double angle, float init, float end) {
             return (angle >= init) && (angle < end);
-        }}}
+        }
+    }
+}
